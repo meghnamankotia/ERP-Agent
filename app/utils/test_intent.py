7ba553a90c02ef2f666d7535cf1371d873e8f7c8
unknown <meghnamankotia@gmail.com>
2026-02-13 10:52:24 +0530

    Added input handler for images

--- app/utils/test_intent.py
+++ app/utils/test_intent.py
@@ -1,5 +1,8 @@
 #input handling to check for intent
+import base64
+from openai import OpenAI
 from unstructured.partition.pdf import partition_pdf_or_image
+from unstructured.partition.xlsx import partition_xlsx
 from unstructured.staging.base import elements_to_text
 from ..schemas.base import Student
 
@@ -8,7 +11,30 @@ def pdfInput(path):
     return elements_to_text(elements)
 
 def xlsInput(path):
-    pass
+    return elements_to_text(partition_xlsx(filename=path))
+
+def imgInput(path):
+    client=OpenAI()
+    with open(path, "rb") as f:
+        image_base64=base64.b64encode(f.read()).decode("utf-8")
+
+    image_input = {
+        "type": "image_url",
+        "image_url": {
+            "url": f"data:image/jpeg;base64,{image_base64}"
+        }
+    }
+
+    response= client.chat.completions.create(
+        model="gpt-4.1",
+        messages=[
+            {"role":"system","content": "Extract text info from the image in a structured format. Do not add supporting text, return only the information"},
+            {"role":"user", "content":[image_input]}
+        ]
+    )
+
+    return response.choices[0].message.content
+
 
 def fileType(path:str):
     #extract file extension
